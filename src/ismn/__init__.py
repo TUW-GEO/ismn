19353b67ec38b72ca85e19659d115f4bbd6d34f3
Wolfgang Preimesberger <wolfgang.preimesberger@geo.tuwien.ac.at>
2021-05-29 00:23:02 +0200

    Update to pyscaffold 4

--- src/ismn/__init__.py
+++ src/ismn/__init__.py
@@ -1,26 +1,16 @@
-# -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
+import sys
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+else:
+    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
-
-import os
-
-# path into src
-src_path = os.path.join(os.path.dirname(__file__), "..")
-
-# path to tests dir, if it exists
-tests_path = os.path.join(src_path, "..", "tests")
-if not os.path.exists(tests_path):
-    tests_path = "unknown"
-
-# path to testsdata dir, if it exists
-testdata_path = os.path.join(src_path, "..", "tests", "test_data")
-if not os.path.exists(testdata_path):
-    testdata_path = "unknown"
+    del version, PackageNotFoundError
